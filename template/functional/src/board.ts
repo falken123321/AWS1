aeea60e9585cb325f4b01ed2e620b92ee1625f9d
Mark Hjuler Pedersen <73698747+BuilderMARK@users.noreply.github.com>
2023-10-17 22:03:33 +0200

    Skal bygges sammen med match som andreas har lavet

--- template/functional/src/board.ts
+++ template/functional/src/board.ts
@@ -69,8 +69,15 @@ export function piece<T>(board: Board<T>, position: Position): T | undefined {
   return board.tiles[position.row * board.width + position.col];
 }
 
-export function matchCheck<T>(board: Board<T>, first: Position, second: Position): Match<T> {
-  if (board.tiles[first.row * board.width + first.col] === board.tiles[second.row * board.width + second.col]) {
+export function matchCheck<T>(
+  board: Board<T>,
+  first: Position,
+  second: Position
+): Match<T> {
+  if (
+    board.tiles[first.row * board.width + first.col] ===
+    board.tiles[second.row * board.width + second.col]
+  ) {
     return {
       matched: board.tiles[first.row * board.width + first.col],
       positions: [first, second],
@@ -83,14 +90,16 @@ export function matchCheck<T>(board: Board<T>, first: Position, second: Position
   }
 }
 
-export function invalidMovesCheck<T>(first: Position, second: Position): boolean {
+export function invalidMovesCheck<T>(
+  first: Position,
+  second: Position
+): boolean {
   //Sikre at man ikke kan lave moves på forskellige rows og cols
   if (first.row !== second.row && first.col !== second.col) {
     return true;
   } else {
     return false;
   }
-
 }
 
 export function canMove<T>(
@@ -99,25 +108,14 @@ export function canMove<T>(
   second: Position
 ): boolean {
   if (
-    (!isPositionWithinBoardBounds(board, first) ||
-      !isPositionWithinBoardBounds(board, second)) ||
+    !isPositionWithinBoardBounds(board, first) ||
+    !isPositionWithinBoardBounds(board, second) ||
     matchCheck(board, first, second).matched !== undefined || // Sikre man ikke må move en tile hvis ikke det resultere i et match
     invalidMovesCheck(first, second)
-
   ) {
     return false;
   }
   return true;
-}
-
-function isPositionWithinBoardBounds<T>(
-  board: Board<T>,
-  position: Position
-): boolean {
-  const isRowValid = position.row >= 0 && position.row < board.height;
-  const isColValid = position.col >= 0 && position.col < board.width;
-
-  return isRowValid && isColValid;
 }
 
 export function move<T>(
@@ -130,5 +128,57 @@ export function move<T>(
     return { board, effects: [] };
   }
 
-  return { board, effects: [] };
+  // Swap the tiles at the first and second positions
+  const temp = board.tiles[first.row * board.width + first.col];
+  board.tiles[first.row * board.width + first.col] =
+    board.tiles[second.row * board.width + second.col];
+  board.tiles[second.row * board.width + second.col] = temp;
+
+  const effects = refill(generator, board);
+
+  return { board, effects };
+}
+
+function isPositionWithinBoardBounds<T>(
+  board: Board<T>,
+  position: Position
+): boolean {
+  const isRowValid = position.row >= 0 && position.row < board.height;
+  const isColValid = position.col >= 0 && position.col < board.width;
+
+  return isRowValid && isColValid;
+}
+
+//Denne funktion virker måske  Cascading
+// registers if refilling brings new matches
+// iterates until there are no new matches
+function refill<T>(generator: Generator<T>, board: Board<T>): Effect<T>[] {
+  const effects: Effect<T>[] = [];
+  let foundMatch = true;
+  while (foundMatch) {
+    foundMatch = false;
+
+    const matches: Match<T>[] = [];
+
+    for (const match of matches) {
+      effects.push({
+        kind: "Match",
+        match: match,
+      });
+      for (const position of match.positions) {
+        board.tiles[position.row * board.width + position.col] =
+          undefined as any;
+      }
+      foundMatch = true;
+    }
+
+    for (let i = 0; i < board.tiles.length; i++) {
+      if (board.tiles[i] === undefined) {
+        board.tiles[i] = generator.next();
+        effects.push({ kind: "Refill" });
+      }
+    }
+  }
+
+  return effects;
 }
