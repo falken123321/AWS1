261aaa11d015e77e7180c7c8cba45f4427f47813
Mark Hjuler Pedersen <73698747+BuilderMARK@users.noreply.github.com>
2023-10-17 12:33:02 +0200

    virker ik

--- template/functional/src/board.ts
+++ template/functional/src/board.ts
@@ -1,32 +1,89 @@
-export type Generator<T>= { next:() => T } 
+export interface Position {
+  row: number;
+  col: number;
+}
 
-export type Position = {
-    row: number,
-    col: number
-}    
+export interface Board<T> {
+  width: number;
+  height: number;
+  tiles: T[];
+}
 
-export type Match<T> = {
-    matched: T,
-    positions: Position[]
-}    
+export interface Effect<T> {
+  kind: "Match" | "Refill";
+  match?: {
+    matched: T;
+    positions: Position[];
+  };
+}
 
-export type Board<T> = ?;
-
-export type Effect<T> = ?;
+export interface Generator<T> {
+  next(): T;
+}
 
 export type MoveResult<T> = {
-    board: Board<T>,
-    effects: Effect<T>[]
-}    
+  board: Board<T>;
+  effects: Effect<T>[];
+};
 
-export function create<T>(generator: Generator<T>, width: number, height: number): Board<T> {
-}    
-
-export function piece<T>(board: Board<T>, p: Position): T | undefined {
-}    
-
-export function canMove<T>(board: Board<T>, first: Position, second: Position): boolean {
+export function create<T>(
+  generator: Generator<T>,
+  width: number,
+  height: number
+): Board<T> {
+  const tiles: T[] = [];
+  for (let i = 0; i < width * height; i++) {
+    tiles.push(generator.next());
+  }
+  return {
+    width,
+    height,
+    tiles,
+  };
 }
 
-export function move<T>(generator: Generator<T>, board: Board<T>, first: Position, second: Position): MoveResult<T> {
+export function positions<T>(board: Board<T>): Position[] {
+  const positions: Position[] = [];
+  for (let row = 0; row < board.height; row++) {
+    for (let col = 0; col < board.width; col++) {
+      positions.push({ row, col });
+    }
+  }
+  return positions;
 }
+
+export function piece<T>(board: Board<T>, position: Position): T | undefined {
+  if (
+    position.row < 0 ||
+    position.row >= board.height ||
+    position.col < 0 ||
+    position.col >= board.width
+  ) {
+    return undefined;
+  }
+  return board.tiles[position.row * board.width + position.col];
+}
+
+export function canMove<T>(
+  board: Board<T>,
+  first: Position,
+  second: Position
+): boolean {
+  const canMoveOneStep =
+    (Math.abs(first.row - second.row) === 1 && first.col === second.col) ||
+    (Math.abs(first.col - second.col) === 1 && first.row === second.row);
+
+  if (canMoveOneStep) {
+    if (board[second.row][second.col] !== null) return false;
+    return true;
+  }
+
+  return false;
+}
+
+export function move<T>(
+  generator: Generator<T>,
+  board: Board<T>,
+  first: Position,
+  second: Position
+): MoveResult<T> {}
